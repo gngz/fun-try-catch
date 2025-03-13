a18d56fb13b45033907ec089eca7aeac31806c80
Gonçalo Passos <dev@diogopassos.pt>
2025-03-13 23:22:34 +0000

    chore: init repo

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/try-catch.ts"],
+  splitting: false,
+  sourcemap: true,
+  format: "esm",
+  minify: true,
+  dts: true,
+  clean: true,
+  target: "es2015",
+});
