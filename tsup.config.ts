3ca25888a986b956fa9a5434c9d0c378edede3ce
Gonçalo Passos <dev@diogopassos.pt>
2025-03-13 23:45:45 +0000

    chore: format configs

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/try-catch.ts"],
+  entry: ['src/try-catch.ts'],
   splitting: false,
   sourcemap: true,
-  format: "esm",
+  format: 'esm',
   minify: true,
   dts: true,
   clean: true,
-  target: "es2015",
+  target: 'es2015',
 });
